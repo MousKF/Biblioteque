6479b038904b105ed5292a4c8a0dc1b2080f5f3e
MousKF <valentin59ortiz@outlook.fr>
2025-06-16 16:30:01 +0200

    Javascrpt

---
+++ Javascript/Java.js
@@ -0,0 +1,39 @@
+let targetNumer = 113;
+
+
+let userNumber = prompt('Devine-Moi! Je me situe entre 10 et 150')
+while (userNumber != targetNumer) {
+    if(userNumber > targetNumer) {
+    alert( "Trop Grand!")
+    } else{
+    alert("Trop Petit!")
+    }
+     userNumber = prompt('Devine-Moi! Je me situe entre 10 et 150')
+}
+alert(`Gg , vas bosser maintenant!`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let firstname = prompt('Quel est votre prénom?');
+let lastname = prompt('Quel est votre nom?');
+
+alert(`Bonjour ${firstname} ${lastname} !`);
+*/
+
+
