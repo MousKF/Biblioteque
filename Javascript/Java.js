df391acac2797e20393d5169d405937df986b951
MousKF <valentin59ortiz@outlook.fr>
2025-06-17 11:47:04 +0200

    deuxieme javascript

--- Javascript/Java.js
+++ Javascript/Java.js
@@ -1,16 +1,37 @@
-let targetNumer = 113;
+let trigger = document.querySelector("#send-button");
+let numberzone = document.querySelector('#user-number');
+let displayzone = document.querySelector('#game-display');
+let targetnumber = Math.floor(Math.random()*100)+1;
 
+console.log(trigger, numberzone, displayzone, targetnumber);
 
-let userNumber = prompt('Devine-Moi! Je me situe entre 10 et 150')
-while (userNumber != targetNumer) {
-    if(userNumber > targetNumer) {
-    alert( "Trop Grand!")
-    } else{
-    alert("Trop Petit!")
+function comparenumber() {
+   // console.log('ca marche');
+
+let userNumber = numberzone.value;
+let result;
+//console.log(usernumber)
+    if(userNumber==targetnumber){
+       result = 'Gg!';
+      }  else if(userNumber > targetnumber)
+        result = `c'est trop grand!`;
+    else {
+        result = `c'est trop petit!`;
     }
-     userNumber = prompt('Devine-Moi! Je me situe entre 10 et 150')
+
+    let resultzone = document.createElement(`p`);
+    resultzone.innerHTML = result;
+    resultzone.style.padding = "5px"
+    resultzone.classList.add(`result-line`)
+    
+
+
+
+    //console.log(resultzone);
+
+    displayzone.appendChild(resultzone);
 }
-alert(`Gg , vas bosser maintenant!`);
+trigger.addEventListener(`click`, comparenumber);
 
 
 
